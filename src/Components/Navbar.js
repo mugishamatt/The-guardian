a4f59f80e0c0c42560a973cc77b5b3e26c88c9ae
mugishamatt <mugisham410@gmail.com>
2020-08-09 20:35:25 -0500

    added Navbar classComponent

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+
+import React, { Fragment } from 'react';
+
+class Navbar extends React. Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+                <div>
+                    <h1>Support The Gardian</h1>
+                    <p> Available for EveryOne, funded by readers</p>
+                </div>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Navbar;
