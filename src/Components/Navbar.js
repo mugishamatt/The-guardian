be56f2181d4fed99407bb7310a58d3383f4cdaa5
mugishamatt <mugisham410@gmail.com>
2020-08-10 21:14:30 -0500

    instaled router

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,37 @@
 
 import React, { Fragment } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
-class Navbar extends React. Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <React.Fragment >
-                <div className="header">
-                    <div className="Support-div">
-                    <h1 className="Support-text1">Support The Gardian</h1>
-                    <p className="Support-text2"> Available for EveryOne, funded by readers</p>
+class Navbar extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {}
+        }
+        render() {
+            return (
+                <React.Fragment >
+                    <div className="header">
+                        <div className="Support-div">
+                            <h1 className="Support-text1">Support The Gardian</h1>
+                            <p className="Support-text2"> Available for EveryOne, funded by readers</p>
+                        </div>
+                       {/* route */}
+                        <Router>
+                        <div className="subsnavbar">
+                          <Link  clasName="contribute" to = "#">Conttribute</Link>
+                          <Link ClassName= "subscribe" to = "#">Subcribe</Link>
+                        </div>
+                        </Router>
                     </div>
-                </div>
-            </React.Fragment>
-         );
+
+                </React.Fragment>
+            );
+        }
     }
-}
- 
+
 export default Navbar;
